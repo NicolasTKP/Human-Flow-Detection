d90f54a4ed0eaa3f0fa28323174b487ac12819c2
daphnengkq <nkqing1009@gmail.com>
2025-05-05 15:33:13 +0800

    test.py

--- test.py
+++ test.py
@@ -1,22 +1,29 @@
-from ultralytics import YOLO
-import cv2
+# from ultralytics import YOLO
+# import cv2
 
-model = YOLO(".\\runs\\detect\\train\\weights\\best.pt")
-# results = model("186.png", save=True, conf=0.5)
+# model = YOLO(".\\runs\\detect\\train\\weights\\best.pt")
+# # results = model("186.png", save=True, conf=0.5)
 
 
-cap = cv2.VideoCapture(0) 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+# cap = cv2.VideoCapture(0) 
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     if not ret:
+#         break
 
-    results = model(frame, conf=0.5) #predict
+#     results = model(frame, conf=0.5) #predict
 
-    cv2.imshow("Human Detection", results[0].plot())
+#     cv2.imshow("Human Detection", results[0].plot())
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("constantinwerner/human-detection-dataset")
+
+print("Path to dataset files:", path)
